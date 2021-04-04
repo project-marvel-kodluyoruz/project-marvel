0d2bb94156a7acb81d81fe79430943f609084d01
klcysn <klcysn@users.noreply.github.com>
2021-04-04 19:35:03 +0200

    make configuration

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,12 @@
-import React from "react"
+import React, {useState} from "react"
+import fetchData from "../../helpers/fetchData"
 
 const Home = ()=>{
+    const [data, setData] = useState([])
+    console.log(data)
     return(
         <div>
-            Home
+            <button onClick={()=>fetchData("series", "", "").then(data=>setData(data))}>Fetch Data</button>
         </div>
     )
 }

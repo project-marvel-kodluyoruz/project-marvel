bd36eff419b678667c7b5d183b91511f78064851
klcysn <klcysn@users.noreply.github.com>
2021-04-05 19:10:10 +0200

    test

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -8,13 +8,7 @@ const Home = ()=>{
     const [data, setData] = useState([])
     const [force, setForce] = useState(0)
 
-    // test test
-    // test test
-    
-     // test test
-    // test test
-     // test test
-    // test test
+
 
     // useEffect(()=>{
     //     handleGetData().then((data)=>setData(data ? data : []))

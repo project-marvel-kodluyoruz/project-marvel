d8342941c28c10d1433823e10a0269f3172103ba
klcysn <klcysn@users.noreply.github.com>
2021-04-06 16:04:06 +0200

    Make all data fetch

--- src/helpers/fetchData.js
+++ src/helpers/fetchData.js
@@ -9,7 +9,20 @@ export default async function fetchData(context, id, detail, limit){
     const privateKey = process.env.REACT_APP_PRIVATKEY;
     const ts = process.env.REACT_APP_TS;
     const auth = `ts=${ts}&apikey=${publicKey}&hash=${md5(`${ts}${privateKey}${publicKey}`)}`;
+
+    if(id || limit){
+        const {data:{data:{results}}} = await axios.get(`${API_URL}/${context}${id ? `/${id}` : ""}${detail ? `/${detail}` : ""}?${auth}&limit=${limit ? limit : 100}`)
+        return results
+    }else{
+        let offset = 0
+        let characterList = []
+        while(offset < 1400){
+            const {data:{data:{results}}} = await axios.get(`${API_URL}/${context}${id ? `/${id}` : ""}${detail ? `/${detail}` : ""}?${auth}&limit=${100}&offset=${offset}`)
+            characterList = await [...characterList, ...results]
+            offset += 100
+        }
+        return characterList
+    }
     
-    const {data:{data:{results}}} = await axios.get(`${API_URL}/${context}${id ? `/${id}` : ""}${detail ? `/${detail}` : ""}?${auth}&limit=${limit ? limit : 100}`)
-    return results
+    
 }

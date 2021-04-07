cf28f8c026a51de462bd421d71a2d4c7128b4805
klcysn <klcysn@users.noreply.github.com>
2021-04-07 09:08:58 +0200

    Add username and avatar

--- src/Components/Navbar/Nav.js
+++ src/Components/Navbar/Nav.js
@@ -1,16 +1,22 @@
-import react from "react"
-import { Navbar, Nav, Form, FormControl } from 'react-bootstrap'
+import { Navbar, Nav, Form, FormControl, Image } from 'react-bootstrap'
 import "./Navbar.scss"
 import React from 'react'
 import { Animated } from "react-animated-css";
 import {useHistory} from "react-router-dom"
+import {useSelector} from "react-redux"
+import {logout} from "../../authentication/Authentication"
 
 export default function NavbarComponents() {
     const history = useHistory()
-
+    const user = useSelector(state => state.userId)
     const handleRoute = (event) => {
         history.push(`/${event.target.name}`)
     }
+    const handleLogOut = () =>{
+        logout()
+        history.push("/")
+    }
+    console.log(user)
     return (
         <div className="NavbarComponent">
             
@@ -23,8 +29,19 @@ export default function NavbarComponents() {
                     <Nav.Link className="NavText" href="#pricing">Movies</Nav.Link>
                 </Nav>
                 <Form inline>
+                    {!user
+                    ?
+                    <>
                     <Nav.Link className="NavText" name="login" onClick={handleRoute}>LOGİN</Nav.Link>
                     <Nav.Link className="NavText" name="register" onClick={handleRoute}>SİGN UP</Nav.Link>
+                    </>
+                    :
+                    <>
+                    <Nav.Link className="NavText" onClick={handleLogOut}>LOGOUT</Nav.Link>
+                    <Nav.Link className="NavText user-name">{user.displayName[0].toUpperCase()}{user.displayName.slice(1)}</Nav.Link>
+                    <Image className="avatar" src="https://static.wikia.nocookie.net/marvelcinematicuniverse/images/3/36/Captain_America_Shield.png/revision/latest/scale-to-width-down/310?cb=20190316204818" roundedCircle />
+                    </>
+                    }
                 </Form>
                
             </Navbar>
--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -19,6 +19,7 @@ const Register = () => {
       .string()
       .required("Please enter your password")
       .oneOf([yup.ref("password"), null], "Passwords must match"),
+    displayName: yup.string().required()
   });
   return (
     <div className="register-body">
@@ -27,10 +28,10 @@ const Register = () => {
         <img className="form-background-image" src="https://www.enjpg.com/img/2020/iron-man-8-scaled.jpg" alt="" />
         <p className="accordion-button" onClick={() => setShow((s) => !s)}> {show ? "-" : "+"}</p>
         <Formik
-          initialValues={{ email: "", password: "", confirmPassword: "" }}
+          initialValues={{ displayName: "", email: "", password: "", confirmPassword: "" }}
           validationSchema={schema}
           onSubmit={(values, { setSubmitting }) => {
-            register(values.email.toLowerCase().trim(), values.password);
+            register(values.email.toLowerCase().trim(), values.password, values.displayName.trim().toLowerCase());
             setSubmitting(false);
             history.push("/")
           }}>
@@ -47,6 +48,15 @@ const Register = () => {
           }) =>
             show && (
               <form onSubmit={handleSubmit} className="register-form">
+                <input
+                  type="displayName"
+                  name="displayName"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.displayName}
+                  placeholder="Please enter your user name"
+                  className="register-input" />
+                <span className="register-span">{errors.displayName && touched.displayName && errors.displayName}</span>
                 <input
                   type="email"
                   name="email"
